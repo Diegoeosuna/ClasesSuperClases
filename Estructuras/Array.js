46e8f40e3af1ddf73a4e42d5245ee9f3521ccac3
Diegoeosuna <diegoemilioosuna@hotmail.com>
2023-03-27 21:27:32 -0600

    Último commit de la clase de Encapsulamiento

---
+++ Estructuras/Array.js
@@ -0,0 +1,23 @@
+class MyArray {
+    constructor (){
+        this.length = 0;
+        this.data = {}
+    }
+    //Obtener un elemento, dada una posición / índice.
+
+    get(index) {
+        return this.data[index]
+    }
+
+    push (item) {
+        this.data[this.length] = item
+        this.length++
+        return this.data
+    }
+}
+
+const list  = new MyArray()
+
+list.push("Hola")
+list.push("Mundo")
+console.log(list.get(1));
